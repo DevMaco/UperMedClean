ac9793777696e65d3d9e825a19099a79045ab398
Pierre-Richard Pascal <89923424+Pascal95@users.noreply.github.com>
2023-11-24 12:54:24 +0100

    api Connexion Inscription

--- src/api/User.js
+++ src/api/User.js
@@ -25,5 +25,83 @@ async function getToken(login, password) {
         }
 }
 
+async function GetProfile(token) {
+    try {
+        const endpoint = url + "/users/profile"
+        const response = await fetch(endpoint, {
+        method: 'GET',
+        headers: {
+          'Authorization': `Bearer ${token}`
+        }
+        });
+  
+        if (!response.ok) {
+            throw new Error(`Erreur HTTP: ${response.status}`);
+        }
+        return await response.json();
+    } catch (error) {
+      console.error("Erreur lors de l'appel API avec le token:", error);
+    }
+}
 
+async function RegCNX(login, password) {
+    try {
+        const endpoint = url + "/users/register"
+        const response = await fetch(endpoint, {
+            method: 'POST',
+            headers: {
+            'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+            login: login,
+            password: password
+            })
+        });
+    
+        if (!response.ok) {
+            throw new Error(`Erreur HTTP: ${response.status}`);
+        }
+  
+        const data = await response.json();
+        return data;
+        } catch (error) {
+        console.error("Erreur lors de l'appel API:", error);
+        }
+}
+
+
+async function RegUSR(nom,prenom, adresse, ville, codepostal, mailcontact, telephone, role, idCNX, signature,idFicheMere,numSS) {
+    try {
+        const endpoint = url + "/users/ficheuser"
+        const response = await fetch(endpoint, {
+            method: 'POST',
+            headers: {
+            'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                nom:nom,
+                prenom:prenom, 
+                adresse:adresse, 
+                ville:ville, 
+                codepostal:codepostal, 
+                mailcontact:mailcontact, 
+                telephone:telephone, 
+                role:role, 
+                idCNX:idCNX, 
+                signature:signature,
+                idFicheMere:idFicheMere,
+                numSS:numSS
+            })
+        });
+    
+        if (!response.ok) {
+            throw new Error(`Erreur HTTP: ${response.status}`);
+        }
+  
+        const data = await response.json();
+        return data;
+        } catch (error) {
+        console.error("Erreur lors de l'appel API:", error);
+        }
+}
   
