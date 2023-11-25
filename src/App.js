8dc546abaf8cb4167f37cfe46d00a750f2720104
Jean-Marc Guillaume <jeanmarc.guillaume17@gmail.com>
2023-11-25 18:27:17 +0100

    Routes ok

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import Login from "./components/Login";
 import Signin from "./components/Signin";
 import HomePage from "./components/HomePage";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div>
-     <Login/>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+            <Route path="/Signin" element={<Signin />} />
+                <Route path="/Home" element={<HomePage />} />
+      </Routes>
+    </Router>
   );
 }
-
 export default App;
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,35 @@
 import React from "react";
 import loginImg from '../asset/UperLogin.jpg'
+import { Link } from 'react-router-dom';
 
 
-export default function Login() {
+function RegisterUser() {
+  const [email, setEmail] = useState(''); // Variable1
+  const [password, setPassword] = useState(''); // Variable2
+
+  const handleRegister = async () => {
+    try {
+      const apiURL = 'https://backupper.onrender.com/api/users/register';
+      const response = await fetch(apiURL, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email, password }),
+      });
+
+      if (response.ok) {
+        console.log('Utilisateur enregistré avec succès!');
+      } else {
+        console.error(`Erreur lors de l'enregistrement de l'utilisateur. Code de statut : ${response.status}`);
+        const errorMessage = await response.text();
+        console.error(errorMessage);
+      }
+    } catch (error) {
+      console.error('Une erreur s\'est produite lors de la requête :', error);
+    }
+  };
+
     return (
 
         <div className='grid grid-cols-1 sm:grid-cols-2 h-screen w-full'>
@@ -20,22 +47,23 @@ export default function Login() {
                     <label>
                         Nom
                     </label>
-                   <input className='rounded-lg bg-white-800 mt-2 p-2 focus:border-blue-500' type="text"/>
+                   <input className='rounded-lg bg-white-800 mt-2 p-2 focus:border-blue-500 text-gray-900' type="text"/>
                 </div>
-                   <div className='flex flex-col text-gray-900' py-2>
+                   <div className='flex flex-col text-gray-100' py-2>
                     <label>
                         Mot de passe
                     </label>
-                   <input className='rounded-lg bg-white-800 mt-2 p-2 focus:border-blue-500 ' type="password"/>
+                   <input className='rounded-lg bg-white-800 mt-2 p-2 focus:border-blue-500 text-gray-900' type="password"/>
                 </div>
                 <div className='flex justify-between text-gray-100 py-2'>
  <p className="flex items-center"><input type="checkbox" />Rester connecté</p>
  <p>Mot de passe oublié</p>
                 </div>
+                <Link to="Signin">
                <button className='w-full my-2 py-2 bg-teal-800 shadow-lg shadow-teal-500/20 text-white font-semibold rounded-lg'>Se connecter</button>
                 <button className='w-full my-2 py-2 bg-yellow-500 shadow-lg shadow-teal-500/20 text-white font-semibold rounded-lg'>S'inscrire</button>
                                 <button className='w-full my-2 py-2 bg-yellow-500 shadow-lg shadow-teal-500/20 text-white font-semibold rounded-lg'>Inscrire un patient</button>
-
+</Link>
             </form>
         </div>
         </div>
