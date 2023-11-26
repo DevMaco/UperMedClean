368e18ea9568d09a67b4253435cd57d4bcdd8168
Jean-Marc Guillaume <jeanmarc.guillaume17@gmail.com>
2023-11-26 12:18:57 +0100

    ajoute de variables et de nouveaux champs call api manquant

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,11 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import loginImg from '../asset/UperLogin.jpg'
-import { Link } from 'react-router-dom';
 
 
 function RegisterUser() {
   const [email, setEmail] = useState(''); // Variable1
   const [password, setPassword] = useState(''); // Variable2
+
 
   const handleRegister = async () => {
     try {
@@ -15,7 +15,7 @@ function RegisterUser() {
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ email, password }),
+        body: JSON.stringify({ email, password, }),
       });
 
       if (response.ok) {
@@ -30,42 +30,55 @@ function RegisterUser() {
     }
   };
 
-    return (
+  return (
+    <div className='grid grid-cols-1 sm:grid-cols-2 h-screen w-full'>
+      <div className="hidden sm:block">
+        <img className="w-full h-full object-cover" src={loginImg} alt=""/>
+      </div>
 
-        <div className='grid grid-cols-1 sm:grid-cols-2 h-screen w-full'>
-            
-            <div className="hidden sm:block">
-            <img className="w-full h-full object-cover" src={loginImg} alt=""/>
-        </div>
-
-        <div className='bg-gray-700 flex flex-col justify-center'>
-            <form className='max-w-[400PX] w-full mx-auto bg-gray-800  p-8 px-8  rounded-lg'>
-                <h2 className='text-4xl dark:text-white font bold text-center'>
-                    UPERMED
-                </h2>
-                <div className='flex flex-col text-gray-100' py-2>
-                    <label>
-                        Nom
-                    </label>
-                   <input className='rounded-lg bg-white-800 mt-2 p-2 focus:border-blue-500 text-gray-900' type="text"/>
-                </div>
-                   <div className='flex flex-col text-gray-100' py-2>
-                    <label>
-                        Mot de passe
-                    </label>
-                   <input className='rounded-lg bg-white-800 mt-2 p-2 focus:border-blue-500 text-gray-900' type="password"/>
-                </div>
-                <div className='flex justify-between text-gray-100 py-2'>
- <p className="flex items-center"><input type="checkbox" />Rester connecté</p>
- <p>Mot de passe oublié</p>
-                </div>
-                <Link to="Signin">
-               <button className='w-full my-2 py-2 bg-teal-800 shadow-lg shadow-teal-500/20 text-white font-semibold rounded-lg'>Se connecter</button>
-                <button className='w-full my-2 py-2 bg-yellow-500 shadow-lg shadow-teal-500/20 text-white font-semibold rounded-lg'>S'inscrire</button>
-                                <button className='w-full my-2 py-2 bg-yellow-500 shadow-lg shadow-teal-500/20 text-white font-semibold rounded-lg'>Inscrire un patient</button>
-</Link>
-            </form>
-        </div>
-        </div>
-    )
+      <div className='bg-gray-700 flex flex-col justify-center'>
+        <form className='max-w-[400PX] w-full mx-auto bg-gray-800  p-8 px-8  rounded-lg'>
+          <h2 className='text-4xl dark:text-white font-bold text-center'>
+            UPERMED
+          </h2>
+          
+          <div className='flex flex-col text-gray-100 py-2'>
+            <label>
+              Adresse e-mail
+            </label>
+            {/* Utilisation du champ de texte lié à la variable d'état 'email' */}
+            <input
+              className='rounded-lg bg-white-800 mt-2 p-2 focus:border-blue-500 text-gray-900'
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className='flex flex-col text-gray-100 py-2'>
+            <label>
+              Mot de passe
+            </label>
+            {/* Utilisation du champ de texte lié à la variable d'état 'password' */}
+            <input
+              className='rounded-lg bg-white-800 mt-2 p-2 focus:border-blue-500 text-gray-900'
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <div className='flex justify-between text-gray-100 py-2'>
+            <p className="flex items-center">
+              <input type="checkbox" />Rester connecté
+            </p>
+            <p>Mot de passe oublié</p>
+          </div>
+          <button onClick={handleRegister} className='w-full my-2 py-2 bg-teal-800 shadow-lg shadow-teal-500/20 text-white font-semibold rounded-lg'>Se connecter</button>
+          <button className='w-full my-2 py-2 bg-yellow-500 shadow-lg shadow-teal-500/20 text-white font-semibold rounded-lg'>S'inscrire</button>
+          <button className='w-full my-2 py-2 bg-yellow-500 shadow-lg shadow-teal-500/20 text-white font-semibold rounded-lg'>Inscrire un patient</button>
+        </form>
+      </div>
+    </div>
+  );
 }
+
+export default RegisterUser;
